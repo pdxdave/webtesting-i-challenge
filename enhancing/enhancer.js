a7567a6469f9fea8204baf07cf85b80edd038888
kamaboko1 <github65@hotmail.com>
2019-06-24 13:00:25 -0700

    outline project goals in enhancer.js

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,3 +1,21 @@
+
+/*  Breakdown of the assignment
+1. Keep track of name, durability and enhancement
+2. Enhancement has a range from 0 to 20 
+   Durability has a range from 0 to 100 
+3. SUCCESS
+   Item enhancement + 1 
+   If item enhancement is 20, no level change
+   Durability does not change
+4. FAILURE
+   If item enhancement < 15, the durability is - 5. ** Durability must be >= 5 to avoid neg number
+   if item enhancement is 15 or 16, the durability is - 10 
+   if item enhancement is > 16, enhancement + 1 
+5. REPAIR
+   Durability is set to 100
+*/
+
+
 module.exports = {
   succeed,
   fail,
